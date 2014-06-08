d3778404d11fe33de75a5b892c9a6d6d6d59b824
Raynos <raynos2@gmail.com>
2014-06-08 13:52:29 -0700

    REAL TESTS IN DIS BIETCH

--- test/index.js
+++ test/index.js
@@ -1,8 +1,32 @@
-var test = require("tape")
+var test = require("tape");
+var MockRequest = require("hammock/request");
+var MockResponse = require("hammock/response");
 
-var routes-router = require("../index")
+var Router = require("../index")
 
 test("routes-router is a function", function (assert) {
-    assert.equal(typeof routes-router, "function")
+    assert.equal(typeof Router, "function")
     assert.end()
 })
+
+test("can create a router", function (assert) {
+    var router = Router()
+
+    router.addRoute("/foo", function (req, res) {
+        res.end("bar")
+    })
+
+    router(
+        new MockRequest({ url: "/foo" }),
+        new MockResponse(function (err, resp) {
+            assert.ifError(err)
+
+            assert.equal(resp.body, "bar")
+
+            assert.end()
+        })
+    )
+})
+
+require("./simple-routes.js")
+require("./child-routes.js")
